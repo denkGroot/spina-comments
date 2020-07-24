f2cf588afe6c20cc2bbf7b7ccfd4b67f9a0f3c54
Bram Jetten <mail@bramjetten.nl>
2020-07-24 12:28:34 +0200

    Edit comments

--- app/controllers/spina/comments/admin/comments_controller.rb
+++ app/controllers/spina/comments/admin/comments_controller.rb
@@ -7,6 +7,16 @@ module Spina::Comments
         @comments = Comment.newest_first.page(params[:page]).per(25)
       end
 
+      def edit
+        @comment = Comment.find(params[:id])
+      end
+
+      def update
+        @comment = Comment.find(params[:id])
+        @comment.update(comment_params)
+        redirect_to spina.comments_admin_comments_path
+      end
+
       def approve
         @comment = Comment.find(params[:id])
         @comment.update(approved: true)
@@ -20,6 +30,10 @@ module Spina::Comments
       end
 
       private
+
+        def comment_params
+          params.require(:comment).permit(:body)
+        end
 
         def set_breadcrumbs
           add_breadcrumb Comment.model_name.human(count: 2)
